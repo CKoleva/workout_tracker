41d96889aa06a01884c3c41cfc2e2ac962922c04
Simona Koleva <simona.koleva@egt-digital.com>
2026-02-04 01:35:56 +0200

    update user schemas

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr
 
 
 class UserBase(BaseModel):
@@ -11,6 +11,4 @@ class UserCreate(UserBase):
 
 class UserRead(UserBase):
     id: int
-
-    class Config:
-        orm_mode = True
+    model_config = ConfigDict(from_attributes=True)
