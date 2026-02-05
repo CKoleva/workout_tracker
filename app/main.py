239b5456461c9b520fa78adb8124936bc37d5fc7
Simona Koleva <simona.koleva@egt-digital.com>
2026-02-05 16:29:15 +0200

    register admin router

--- app/main.py
+++ app/main.py
@@ -3,13 +3,14 @@ from fastapi import FastAPI
 from app.db.database import engine, Base
 from app.db import models
 from app.api.auth import router as auth_router
-from app.api import workouts, dashboard
+from app.api import workouts, dashboard, admin
 
 
 app = FastAPI(title="Workout Tracker")
 app.include_router(auth_router)
 app.include_router(workouts.router)
 app.include_router(dashboard.router)
+app.include_router(admin.router)
 
 Base.metadata.create_all(bind=engine)
 
