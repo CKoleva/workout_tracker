a292062cd1e1a26bbe0a6912926a321e0e58a3ad
Simona Koleva <simona.koleva@egt-digital.com>
2026-02-05 15:58:13 +0200

    register workouts router

--- app/main.py
+++ app/main.py
@@ -3,10 +3,12 @@ from fastapi import FastAPI
 from app.db.database import engine, Base
 from app.db import models
 from app.api.auth import router as auth_router
+from app.api import workouts
 
 
 app = FastAPI(title="Workout Tracker")
 app.include_router(auth_router)
+app.include_router(workouts.router)
 
 Base.metadata.create_all(bind=engine)
 
