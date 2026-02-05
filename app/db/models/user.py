85900d53aa0c5d66de032d888e83517508411b1f
Simona Koleva <simona.koleva@egt-digital.com>
2026-02-05 16:25:18 +0200

    add role field to user model

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -9,5 +9,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, nullable=False, index=True)
     hashed_password = Column(String, nullable=False)
+    role = Column(String, nullable=False, default="user")
 
     workouts = relationship("Workout", back_populates="user")
+
